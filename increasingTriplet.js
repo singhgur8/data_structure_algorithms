ffcdecd1a0642982383800b8f30d3d11c2313ac3
Gurjot Singh <gurjotssingh510@gmail.com>
2020-02-16 14:26:08 -0800

    had to get some hints

---
+++ increasingTriplet.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let firstNumber = Infinity;
+    let secondNumber = Infinity;
+    
+    for (let currentNumber of nums) {
+      if (currentNumber > secondNumber && currentNumber > firstNumber) {
+        return true;
+      }
+      if (currentNumber > firstNumber) {
+        secondNumber = currentNumber;
+      } else {
+        firstNumber = currentNumber;
+      }
+    }
+    return false;
+  };
+
+
+
